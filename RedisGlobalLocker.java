9620476dd9a75fb0cf15fe8ff903334fc5757794
吴起 <wuqi@wdai.com>
2017-10-27 14:36:31 +0800

    [ADD]redis分布式锁

---
+++ RedisGlobalLocker.java
@@ -0,0 +1,97 @@
+/**
+ * Copyright (C), 2011-2017, 微贷网.
+ */
+package com.weidai.ucenterx.common;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.data.redis.core.RedisCallback;
+import org.springframework.data.redis.core.RedisOperations;
+import org.springframework.data.redis.core.SessionCallback;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Component;
+import redis.clients.jedis.Jedis;
+
+import java.util.*;
+
+/**
+ * redis分布式锁
+ * 
+ * @author wuqi 2017/10/10 0010.
+ */
+@Component
+public class RedisGlobalLocker {
+
+    private static final ThreadLocal<Map<String, String>> keyHolder = new ThreadLocal<>();
+
+    private static final Random rnd = new Random();
+
+    @Autowired
+    private StringRedisTemplate redisTemplate;
+
+    @Autowired
+    private SnowballIdGenerator snowballIdGenerator;
+
+    /**
+     * 获取锁
+     * 
+     * @param key
+     * @param expx ex：秒，px：毫秒
+     * @param time
+     */
+    public void lock(String key, final String expx, final long time) {
+        String id = String.valueOf(snowballIdGenerator.nextId());
+        Map<String, String> keyId = new HashMap<>(1);
+        keyId.put(key, id);
+        keyHolder.set(keyId);
+        long timeout = 200L;
+        while (!tryLock(key, id, expx, time)) {
+            try {
+                // 设置线程等待时间窗，取一个公平的随机数，防止出现线程饥饿
+                Thread.sleep(randomLongWithBoundary(timeout));
+            } catch (InterruptedException e) {
+            }
+        }
+    }
+
+    private long randomLongWithBoundary(long max) {
+        long min = 1L;
+        return min + (long) (rnd.nextFloat() * (max - min));
+    }
+
+    private boolean tryLock(String key, String value, final String expx, final long time) {
+        String rs = redisTemplate.execute((RedisCallback<String>) connection -> {
+            Jedis jedis = (Jedis) connection.getNativeConnection();
+            return jedis.set(key, value, "NX", expx.toUpperCase(), time);
+        });
+        if ("OK".equals(rs)) {
+            return true;
+        }
+        return false;
+    }
+
+    public void unlock() {
+        Map<String, String> keyId = keyHolder.get();
+        if (keyId != null) {
+            Optional<Map.Entry<String, String>> optional = keyId.entrySet().stream().findFirst();
+            if (optional.isPresent()) {
+                final String key = optional.get().getKey();
+                final String value = optional.get().getValue();
+                redisTemplate.execute(new SessionCallback<Object>() {
+                    @SuppressWarnings({ "unchecked", "rawtypes" })
+                    @Override
+                    public Object execute(RedisOperations operations) throws DataAccessException {
+                        if (value.equals(redisTemplate.opsForValue().get(key))) {
+                            operations.watch(key);
+                            operations.multi();
+                            redisTemplate.delete(key);
+                            return operations.exec();
+                        }
+                        return null;
+                    }
+                });
+            }
+            keyHolder.remove();
+        }
+    }
+}
---
+++ SnowballIdGenerator.java
@@ -0,0 +1,60 @@
+/**
+ * Copyright (C), 2011-2017, 微贷网.
+ */
+package com.weidai.ucenterx.common;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+
+/**
+ * 参考twitter snowflake算法，使用 时间 + 自增sequence，没有NTP回刷时间的问题，简单粗暴
+ * 将64 bit的long型数字拆成3部分，1 bit符号位：0， 中间31 bit表示时间（精确到秒），后32 bit表示自增sequence
+ * 31 bit的时间可以使用2^31 / (1000 * 3600 * 24 * 365) = 68年，32 bit的sequence可以产生 2^32 - 1 = 42亿多个id，理论上足够使用
+ * 
+ * @author wuqi 2017/10/10 0010.
+ */
+@Component
+public class SnowballIdGenerator {
+
+    private static final String UXID_SEQ_KEY = "uxid_seq";
+
+    /**
+     * 开始时间毫秒值：2017-01-01 00:00:00
+     */
+    private static final long startTime = 1_483_200_000_000L;
+
+    /**
+     * 自增sequence占用的位数
+     */
+    private static final long sequenceBits = 32L;
+
+    @Autowired
+    private StringRedisTemplate redisTemplate;
+    
+    /**
+     * 获取下一个id，同步方法
+     * @return
+     */
+    public long nextId() {
+        long now = getNowMillis();
+        long sequence = getUxidSequence();
+        return (((now - startTime) / 1_000L) << sequenceBits) | sequence;
+    }
+
+    private long getUxidSequence() {
+        return redisTemplate.opsForValue().increment(UXID_SEQ_KEY, 1L);
+    }
+
+    private long getNowMillis() {
+        return System.currentTimeMillis();
+    }
+
+    public static void main(String[] args) {
+        LocalDateTime localDateTime = LocalDateTime.of(2017, 1, 1, 0, 0, 0, 0);
+        System.out.println(localDateTime.atZone(ZoneId.systemDefault()).toInstant().toEpochMilli());
+    }
+}
